08b6daa84fb0f65e2e4987f1dbd30eb5e718761b
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-06-29 12:25:46 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,9 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    // System.out.println(dp.getPrice()); // ← Remove this if you only want the bill
     dp.addTakeaway();
-    dp.addExtraCheese(); // won't add again
-    dp.addTakeaway();    // won't add again
+    dp.addExtraCheese(); // ignored
+    dp.addTakeaway();    // ignored
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
